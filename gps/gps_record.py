ebd17dc104a8ca147aff5d154b4d2c8e409c8c12
Liliya Lyubenova <lilly.lubenova@gmail.com>
2022-03-20 22:50:49 +0200

    [Add] Structure for GPS record

---
+++ gps/gps_record.py
@@ -0,0 +1,20 @@
+import json
+from configparser import ConfigParser
+
+config_object = ConfigParser()
+config_object.read("config.ini")
+device_info = config_object["DEVICEID"]
+
+global DEVICE_ID
+DEVICE_ID = device_info["deviceId"]
+
+
+class GPSRecord:
+    def __init__(self, lat, long):
+        self.lat = lat
+        self.long = long
+
+    def __str__(self):
+        return_value = {"device_id": DEVICE_ID, "location": {
+            "latitude": self.lat, "longitude": self.long}}
+        return json.dumps(return_value)
